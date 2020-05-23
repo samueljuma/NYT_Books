db5b3d9d27db828014e28391f30c30b2ad227360
Paulmburu <paulmburu53@gmail.com>
2020-05-24 00:41:58 +0300

    DTO,s for mapping network data

---
+++ app/src/main/java/tk/paulmburu/nytbooks/network/DataTransferObjects.kt
@@ -0,0 +1,42 @@
+package tk.paulmburu.nytbooks.network
+
+import com.squareup.moshi.JsonClass
+import tk.paulmburu.nytbooks.models.Book
+
+/*   Created by Paul Mburu on 5/24/20.
+ *
+ *   Copyright 2020 Paul Mburu
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+@JsonClass(generateAdapter = true)
+data class NetworkBook(
+    val title: String,
+    val author: String,
+    val description: String
+)
+
+@JsonClass(generateAdapter = true)
+data class NetworkBookContainer(
+    val results: List<NetworkBook>
+)
+
+fun NetworkBookContainer.asDatabaseBookModel(): Array<Book>{
+    return results.map {
+        Book(
+            title = it.title,
+            author = it.author,
+            description = it.description
+        )
+    }.toTypedArray()
+}
+
+
