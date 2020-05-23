fbd89bbd227c24d46e0366aff4d2b77c08f31e19
Paulmburu <paulmburu53@gmail.com>
2020-05-24 01:25:33 +0300

    add NetworkUtil to handle different network states

---
+++ app/src/main/java/tk/paulmburu/nytbooks/utils/NetworkUtils.kt
@@ -0,0 +1,64 @@
+package tk.paulmburu.nytbooks.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkRequest
+import android.os.Build
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import java.net.ConnectException
+import java.net.UnknownHostException
+
+/*   Created by Paul Mburu on 5/24/20.
+ *
+ *   Copyright 2020 Paul Mburu
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+object NetworkUtils {
+
+    fun getNetworkStatus(
+        context: Context
+    ): LiveData<Boolean> {
+        val isAvailableLiveData = MutableLiveData<Boolean>()
+        val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val nr = NetworkRequest.Builder()
+
+        cm.registerNetworkCallback(nr.build(), object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                super.onAvailable(network)
+                isAvailableLiveData.postValue(true)
+            }
+
+            override fun onLost(network: Network) {
+                super.onLost(network)
+                isAvailableLiveData.postValue(false)
+
+            }
+        })
+        return isAvailableLiveData
+    }
+
+}
+
+suspend fun <T> executeNonBlocking(
+    execute: suspend () -> ResultState<T>
+): ResultState<T> {
+    return try {
+        execute()
+    } catch (e: UnknownHostException) {
+        Error<T>(e)
+    } catch (e: ConnectException) {
+        Error<T>(e)
+    }
+
+}
