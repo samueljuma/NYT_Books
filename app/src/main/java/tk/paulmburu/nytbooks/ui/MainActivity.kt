90631fc7803ed94ad90a38129a63fcd68119677f
Paulmburu <paulmburu53@gmail.com>
2020-05-23 23:23:54 +0300

    create ui package

--- app/src/main/java/tk/paulmburu/nytbooks/ui/MainActivity.kt
+++ app/src/main/java/tk/paulmburu/nytbooks/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package tk.paulmburu.nytbooks
+package tk.paulmburu.nytbooks.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import tk.paulmburu.nytbooks.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
